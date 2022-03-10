fbad8d4fc5a22b400dde7cca2f6e4d53402e4e6c
Goyco2 <goycochea.q@iisgalvanimi.edu.it>
2022-03-10 10:31:26 +0000

    gpgppgpgpg

--- appEs3.py
+++ appEs3.py
@@ -6,8 +6,7 @@
 #l'utente sceglie se avere la regioneo capoluogho selezionando un radio button 
 from flask import Flask, render_template, request
 app = Flask(__name__)
-lista = []
-lista.append({'Valle Aosta' : "Aosta", "Piemonte" : "Torino", "Liguria" : "Genova", "Lombardia" : "Milano", "Trentino-Alto Adige" : "Trento", "Veneto" : "Venezia", "Friuli-Venezia Giulia" : "Trieste", "Emilia-Romagna" : "Bologna","Toscana" : "Firenze","Marche" : "Ancona","Lazio" : "Roma","Umbria" : "Perugia","Abruzzo" : "L'Aquila","Molise" : "Campobasso","Campania" : "Napoli","Puglia" : "Bari","Basilicata" : "Potenza","Calabria" : "Catanzaro","Sicilia" :"Palermo","Sardegna" : "Cagliari"})
+dizionario  = {'Valle Aosta' : "Aosta", "Piemonte" : "Torino", "Liguria" : "Genova", "Lombardia" : "Milano", "Trentino-Alto Adige" : "Trento", "Veneto" : "Venezia", "Friuli-Venezia Giulia" : "Trieste", "Emilia-Romagna" : "Bologna","Toscana" : "Firenze","Marche" : "Ancona","Lazio" : "Roma","Umbria" : "Perugia","Abruzzo" : "L'Aquila","Molise" : "Campobasso","Campania" : "Napoli","Puglia" : "Bari","Basilicata" : "Potenza","Calabria" : "Catanzaro","Sicilia" :"Palermo","Sardegna" : "Cagliari"}
 @app.route('/', methods=['GET'])
 def hello_world():
     return render_template("datiIta.html")
@@ -15,16 +14,30 @@ def hello_world():
 
 @app.route('/data', methods=['GET'])
 def Data():
-    print(request.args)
-    name = request.args['name']
-    regCap = request.args['regCap']
+    scelta = request.args["Scelta"]
+    if scelta == "R":
+        return render_template("regionees3.html")
+    else:
+        return render_template("capoluogoes3.html")
     
 
-@app.route("/Risposta", methods=["GET"])
-def Risposta():
-    ris = request.args['ris']
-    for utente in lista:
-        if utente[]
+@app.route("/dataReg", methods=["GET"])
+def dataReg():
+    regione = request.args["Regione"]
+    for key, value in dizionario.items():
+        if regione == key:
+            capoluogo = value
+            return render_template("RispostaEs3.html", risp = capoluogo)
+    return "<h1>Errore</h1>"
+
+@app.route("/dataCap", methods=["GET"])
+def dataCap():
+    capoluogo = request.args["Capoluogo"]
+    for key, value in dizionario.items():
+        if capoluogo == value:
+             regione = key
+             return render_template("RispostaEs3.html", risp = regione)
+    return "<h1>Errore</h1>"
 
 
 if __name__ == '__main__':
