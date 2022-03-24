6dd553a90b5acf71b4725c4595066ab31de89f56
Goyco2 <goycochea.q@iisgalvanimi.edu.it>
2022-03-24 10:53:31 +0000

    bo

--- appEs6.py
+++ appEs6.py
@@ -1,10 +1,10 @@
 #realizare un sito web che restituisca la mappa dei quartieri di milano.
 #ci deve essere una home page con un link "quartieri di mialno":
 #cilccando su questo link si devev visualizzare la mappa dei quartieri di milano
-from flask import Flask, render_template, send_file, make_response, url_for, Response
+from flask import Flask, render_template, request, send_file, make_response, url_for, Response
 app = Flask(__name__)
 import io
-import geopandas as gpd
+import geopandas
 import contextily
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
@@ -12,34 +12,81 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
-quartieri = gpd.read_file('/workspace/Flask/ds964_nil_wm')
+milano = geopandas.read_file("/workspace/Flask/ds964_nil_wm")
+
 
 @app.route('/', methods=['GET'])
-def hello_world():
-    return render_template('simple.html')
+def home():
+    return render_template('home_quartieri.html')
+
+@app.route('/visualizza', methods=("POST", "GET"))
+def mappa():
+    return render_template('plot.html')
 
 @app.route('/plot.png', methods=['GET'])
 def plot_png():
-
     fig, ax = plt.subplots(figsize = (12,8))
 
-    quartieri.to_crs(epsg=3857).plot(ax=ax, alpha=0.5)
+    milano.to_crs(epsg=3857).plot(ax=ax, alpha=0.6, edgecolor = "k")
     contextily.add_basemap(ax=ax)
     output = io.BytesIO()
     FigureCanvas(fig).print_png(output)
     return Response(output.getvalue(), mimetype='image/png')
 
 
-@app.route('/plot', methods=("POST", "GET"))
-def mpl():
-    return render_template('plot.html',
-                           PageTitle = "Matplotlib")
+@app.route('/ricerca', methods=("POST", "GET"))
+def search():
+    return render_template('quartiere.html')
 
-@app.route('/data', methods=("GET"))
-def data():
-    quartInp = request.args["quartInp"]
-    df3 = quartieri[quartieri['NIL']== quartInp]
-    return df3.plot
+@app.route('/quartiereInserito', methods=("POST", "GET"))
+def qrtInserito():
+    global user
+    quartieri = [item for item in milano.NIL]
+    user = request.args["Quartiere"]
+    if user in quartieri:
+        return render_template('plot_quartiere.html')
+    else:
+        return ("<h1>Errore, il quartiere inserito non esiste</h1>")
+
+@app.route('/quartiereInserito.png', methods=['GET'])
+def pngQuartiere():
+    mappa_quartiere = milano[milano.NIL == user]
+
+    fig, ax = plt.subplots(figsize = (12,8))
+    mappa_quartiere.to_crs(epsg=3857).plot(ax=ax, alpha=0.6, edgecolor = "k")
+    contextily.add_basemap(ax=ax)
+
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+
+
+@app.route('/scelta', methods=("POST", "GET"))
+def scegli():
+    quart = milano.NIL
+    return render_template('scelta_quartieri.html', quartiere = quart)
+
+@app.route('/immagine_scelta', methods=("POST", "GET"))
+def img_scelta():
+    global input
+    input = request.args["Scelta"]
+    return render_template('plot_scelta.html')
+
+@app.route('/scelta.png', methods=['GET'])
+def dropdown():
+    mappa_quartiere = milano[milano.NIL == input]
+
+    fig, ax = plt.subplots(figsize = (12,8))
+    mappa_quartiere.to_crs(epsg=3857).plot(ax=ax, alpha=0.6, edgecolor = "k")
+    contextily.add_basemap(ax=ax)
+
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+
+@app.route('/Fontanelle.png', methods=['GET'])
+def Fontanelle():
+    
 
 
 if __name__ == '__main__':
