0f9b2ef2a7cf50c945c3cabba273d216a5d39c39
Goyco2 <goycochea.q@iisgalvanimi.edu.it>
2022-04-11 15:25:34 +0000

    verificaC

--- VerificaC/corezzione.py
+++ VerificaC/corezzione.py
@@ -1,44 +1,79 @@
-from flask import Flask,render_template,request,send_file,make_response, url_for, Response,redirect
+from flask import Flask, render_template, request, send_file, make_response, url_for, Response, redirect
 app = Flask(__name__)
 import io
-import os
-import geopandas as gpd
-import contextily as ctx
+import geopandas
+import contextily
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
 
-quartieri = gpd.read_file("/workspace/Flask/VerificaC/ds964_nil_wm/NIL_WM.dbf")
-bus_tram = gpd.read_file('/workspace/Flask/VerificaC/tpl_percorsi_shp (2).zip')
+tram_bus = geopandas.read_file("/workspace/Flask/VerificaC/ds964_nil_wm/NIL_WM.dbf")
+milano = geopandas.read_file("/workspace/Flask/VerificaC/tpl_percorsi_shp (2).zip")
 
-@app.route('/', methods = ["GET"])
-def homepage():
-    return render_template("homeverificaC.html")
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('home.html')
 
 @app.route('/selezione', methods=['GET'])
 def selezione():
-    scelta = request.args["scelta"]
-    if scelta == "es1":
-        return redirect("mid")
-    elif scelta == "es2":
-        return redirect("")
+    scelta = request.args["Scelta"]
+    if scelta == "Es1":
+        return redirect(url_for("lunghezza"))
+    elif scelta == "Es2":
+        return redirect(url_for("ricerca"))
     else:
-        return redirect("")
-    return render_template("a.html")
+        return redirect(url_for("dropdown"))
 
-@app.route('/mid', methods=['GET'])
-def mid():
-    return render_template("numriInp.html")
+@app.route('/lunghezza', methods=['GET'])
+def lunghezza():
+    return render_template("valori.html")
 
-@app.route('/numero', methods=['GET'])
-def numero():
-    valore1 = int(request.args['valore1'])
-    valore2 = int(request.args['valore2'])
-    risultato1 = bus_tram[bus_tram['lung_km']< valore1 & bus_tram['lung_km']> valore2]
-    return render_template('tabel1.html', table = risultato1.to_html())
+@app.route('/lunghezzaCompresa', methods=['GET'])
+def lunghezzaCompresa():
+    val1 = request.args["Valore1"]
+    val2 = request.args["Valore2"]
+    tram_bus_compresi = tram_bus[tram_bus["lung_km"].astype("float") > float(val1)]
+    tram_bus_compresi = tram_bus_compresi[tram_bus_compresi["lung_km"].astype("float") < float(val2)].sort_values(by = "linea", ascending = True)
+    return render_template("elenco_compresi.html", tabella = tram_bus_compresi.to_html())
+
+@app.route('/ricerca', methods=['GET'])
+def ricerca():
+    return render_template("input.html")
+
+@app.route("/lineeQuart", methods=["GET"])
+def lineequart():
+    quartiere = request.args["Quartiere"]
+    mappa_quartiere = milano[milano["NIL"].str.contains(quartiere)]
+    linee_quartiere = tram_bus[tram_bus.intersects(mappa_quartiere.geometry.squeeze())].sort_values(by = "linea", ascending = True)
+    return render_template("tabella_linee.html", tabella = linee_quartiere.to_html())
+
+@app.route('/dropdown', methods=['GET'])
+def dropdown():
+    return render_template("dropdown.html", linee = tram_bus["linea"].drop_duplicates().sort_values(ascending=True))
+
+@app.route("/mappa", methods=["GET"])
+def mappa():
+    global mappa_linea
+    linea = int(request.args["Linea"])
+    mappa_linea = tram_bus[tram_bus["linea"].astype("int") == linea]
+    return render_template("mappa.html")
+
+@app.route("/linea.png", methods=["GET"])
+def lineaPng():
+    fig, ax = plt.subplots(figsize = (12,8))
+
+    mappa_linea.to_crs(epsg=3857).plot(ax=ax, edgecolor="r")
+    milano.to_crs(epsg=3857).plot(ax=ax, alpha=0.6, edgecolor="k")
+    contextily.add_basemap(ax=ax)   
+
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
 
 
-if __name__ == "__main__":
-    app.run(host = '0.0.0.0', port = 3246, debug = True) 
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
